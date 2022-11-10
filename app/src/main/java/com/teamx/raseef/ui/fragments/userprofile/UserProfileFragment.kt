f605effd1033c8874b8e5c259210f8da39ad9034
Hur Rizvi <92028524+jari-rizvi@users.noreply.github.com>
2022-11-10 18:22:16 +0500

    edit profile

--- app/src/main/java/com/teamx/raseef/ui/fragments/userprofile/UserProfileFragment.kt
+++ app/src/main/java/com/teamx/raseef/ui/fragments/userprofile/UserProfileFragment.kt
@@ -1,9 +1,11 @@
 package com.teamx.rassef.ui.fragments.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.databinding.adapters.TextViewBindingAdapter.setText
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.squareup.picasso.Picasso
 import com.teamx.raseef.BR
 import com.teamx.raseef.R
 import com.teamx.raseef.baseclasses.BaseFragment
@@ -22,9 +24,6 @@ class UserProfileFragment() : BaseFragment<FragmentUserProfileBinding, SignupVie
     override val bindingVariable: Int
         get() = BR.viewModel
 
-
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -39,6 +38,9 @@ class UserProfileFragment() : BaseFragment<FragmentUserProfileBinding, SignupVie
 
             mViewDataBinding.userName.setText(personName)
             mViewDataBinding.userPhone.setText(personEmail)
+            Picasso.get().load(personPhoto).into(mViewDataBinding.profilePicture)
+
+            Log.d("photooooo",personPhoto.toString())
 
         }
 
